82d4c799275874e95fcaf194f2f47fccf8b6279c
Victor campos <victor.campos@visagio.com>
2016-01-14 23:32:15 -0200

    adding basic folders

---
+++ lib/mix/tasks/guardian.gen.email_login.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.Guardian.Gen.EmailLogin do
+  use Mix.Task
+
+  @shortdoc "Generate template for guardian email login"
+
+  def run(_) do
+    Mix.shell.info "hello"
+  end
+end
---
+++ test/gen_guardian_test.exs
@@ -0,0 +1,8 @@
+defmodule GenGuardianTest do
+  use ExUnit.Case
+  doctest GenGuardian
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
