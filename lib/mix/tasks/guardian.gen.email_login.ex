b85e17655537616f72d438af2f620b92ae5efb51
Victor campos <victor.campos@visagio.com>
2016-01-16 08:34:10 -0200

    adding guardian_serializer

--- lib/mix/tasks/guardian.gen.email_login.ex
+++ lib/mix/tasks/guardian.gen.email_login.ex
@@ -3,7 +3,54 @@ defmodule Mix.Tasks.Guardian.Gen.EmailLogin do
 
   @shortdoc "Generate template for guardian email login"
 
-  def run(_) do
-    Mix.shell.info "hello"
+  def run(args) do
+    switches = []
+
+    {opts, parsed, _} = OptionParser.parse(args, switches: switches)
+    [singular, plural | attrs] = validate_args!(parsed)
+
+    binding   = Mix.Phoenix.inflect(singular)
+    path      = binding[:path]
+
+    files = [
+      {:eex, "guardian_serializer.ex", "lib/#{path}/guardian_serializer.ex"},
+      {:eex, "migration.exs", "priv/repo/migrations/#{timestamp()}_create_user.exs"}
+    ]
+
+    Mix.Phoenix.copy_from paths(), "priv/templates/guardian.gen.email_login", "", binding, files
+
+    Mix.shell.info """
+    Remember to update your repository by running migrations:
+
+      $ mix ecto.migrate
+    """
+  end
+
+  defp validate_args!([_, plural | _] = args) do
+    cond do
+      String.contains?(plural, ":") ->
+        raise_with_help
+      plural != Phoenix.Naming.underscore(plural) ->
+        Mix.raise "expected the second argument, #{inspect plural}, to be all lowercase using snake_case convention"
+      true ->
+        args
+    end
+  end
+
+  defp raise_with_help do
+    Mix.raise """
+    mix guardian.gen.email_login expects both singular and plural names
+    of the application:
+
+        mix guardian.gen.email_login ApplicationName application_name
+    """
+  end
+
+  defp validate_args!(_) do
+    raise_with_help
+  end
+
+  defp paths() do
+    [".", :gen_guardian]
   end
 end
